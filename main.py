f0053edd42fb221cf4190fdda10dd74cba1ef691
이은지 <dldmswl010101@naver.com>
2026-08-12 20:48:03 +0900

    main.py

---
+++ main.py
@@ -0,0 +1,10 @@
+def main():
+    print("=== Mini NPU Simulator ===")
+    print()
+    print("[모드 선택]")
+    print("1. 사용자 입력 (3x3)")
+    print("2. data.json 분석")
+
+
+if __name__ == "__main__":
+    main()
